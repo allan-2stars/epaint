3aab37558dfe91dd218817744f2f58d85ea416c7
Allan Wang <wvllzh@aol.com>
2021-01-24 18:09:07 +1100

    added ready file for cart item component

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,9 @@
+import './cart-item.styles.scss'
+
+const CartItem = ({item: {imageUrl, price, name}}) => {
+    return ( 
+        <div className="cart-item"></div>
+     );
+}
+ 
+export default CartItem;
